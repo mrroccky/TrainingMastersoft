2541447a1a132716396a3e9e4a9333be197b18f4
Rakesh Bhure <rakeshbhure4@gmail.com>
2025-02-07 20:57:06 +0530

    day 6 task create student grade syatem

--- DevelopmentTask/Task1DesignStudentGradeSystem/Task1DesignStudentGradeSystem/default.aspx.cs
+++ DevelopmentTask/Task1DesignStudentGradeSystem/Task1DesignStudentGradeSystem/default.aspx.cs
@@ -28,9 +28,10 @@ namespace Task1DesignStudentGradeSystem
             string name, branch;
             int rollnumber, subject1marks, subject2marks, subject3marks, subject4marks, subject5marks;
 
-            
+            //getting user data using feilds
             try
             {
+                //assigning data to local variables
                 rollnumber = int.Parse(txtRoll.Text);
                 name = txtName.Text;
                 branch = txtName.Text;
@@ -39,16 +40,19 @@ namespace Task1DesignStudentGradeSystem
                 subject3marks = int.Parse(txtSub3marks.Text);
                 subject4marks = int.Parse(txtSub4marks.Text);
                 subject5marks = int.Parse(txtSub5marks.Text);
-
+                //performing passing marks logic so no student will get result if marks less than 40 
+                //
                 if (subject1marks > passingMarks && subject2marks > passingMarks && subject3marks > passingMarks && subject4marks > passingMarks && subject5marks > passingMarks)
                 {
                     total = subject1marks + subject2marks + subject3marks + subject4marks + subject5marks;
+                    //creating result on click of button so student will get information on text box
                     txtTotalMarks.Text = total.ToString();
                    // calculateGrade(total);
                 }
             }
             catch (Exception)
             {
+                //showing error on same feild if theres any mistacke entering data
                 txtGrade.Text = "Kuthtri Error Aahe Bhau";
             }
         }
@@ -64,12 +68,13 @@ namespace Task1DesignStudentGradeSystem
                 rollnumber = int.Parse(txtRoll.Text);
                 name = txtName.Text;
                 branch = txtName.Text;
+                //data is coming in the form of string so it furter need to converted to perform logical opration
                 subject1marks = int.Parse(txtSub1marks.Text);
                 subject2marks = int.Parse(txtSub2marks.Text);
                 subject3marks = int.Parse(txtSub3marks.Text);
                 subject4marks = int.Parse(txtSub4marks.Text);
                 subject5marks = int.Parse(txtSub5marks.Text);
-
+                //chech that no one will get result if he/she has less mark at any of the subject
                 if (subject1marks > passingMarks && subject2marks > passingMarks && subject3marks > passingMarks && subject4marks > passingMarks && subject5marks > passingMarks)
                 {
                     total = subject1marks + subject2marks + subject3marks + subject4marks + subject5marks;
@@ -81,7 +86,7 @@ namespace Task1DesignStudentGradeSystem
                 txtGrade.Text = "Kuthtri Error Aahe Bhau";
             }
            
-        }
+        }//this function will calcualte grade and show inside text feild naed as txtGrade
         public void calculateGrade(int total)
         {
             if (total>250)
