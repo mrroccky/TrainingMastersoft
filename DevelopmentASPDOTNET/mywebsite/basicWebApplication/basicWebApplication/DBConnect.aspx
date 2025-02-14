54e443d1e2f7efdde13ba6823e6011b169ee158c
Rakesh Bhure <rakeshbhure4@gmail.com>
2025-02-14 12:55:10 +0530

    sql database connection dot net

---
+++ DevelopmentASPDOTNET/mywebsite/basicWebApplication/basicWebApplication/DBConnect.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DBConnect.aspx.cs" Inherits="basicWebApplication.DBConnect" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Database connectivity Implementation<br />
+            <br />
+            Enter roll number:
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            Enter name:
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            Enter city:
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Submit" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ DevelopmentASPDOTNET/mywebsite/basicWebApplication/basicWebApplication/DBConnect.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+// step 1 include database library
+using System.Data.SqlClient;
+
+
+namespace basicWebApplication
+{
+    public partial class DBConnect : System.Web.UI.Page
+    {
+        // step 2 create database object
+        SqlConnection con;
+        SqlCommand com;
+        SqlDataReader dr;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // step 3 break page refreshment
+            if (!IsPostBack)
+            {
+                //  
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            // step 1 open database
+            string path = "Database connection";
+            con = new SqlConnection(path);
+
+            con.Open();
+
+            // step 2 apply command
+            string k = "INSERT query";
+            com = new SqlCommand(k, con); // variable, connection object
+
+            // step 3 execute command
+            com.ExecuteNonQuery(); 
+        }
+    }
+}
